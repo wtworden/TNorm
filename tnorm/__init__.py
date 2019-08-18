11e5e8dc0e6526cf00800ad69f28e808d1300e3c
W Worden <you@example.com>
2019-08-18 15:40:25 -0500

    fixed imports (maybe..)

--- tnorm/__init__.py
+++ tnorm/__init__.py
@@ -2,6 +2,9 @@ name = "tnorm"
 
 from tnorm.version import __version__
 
+import tnorm.kernel
+import tnorm.GUI
+
 from tnorm.TN_wrapper import TN_wrapper as load
-from tnorm.kernel.matrices import sign_matrix
+from tnorm.kernel import sign_matrix
 
